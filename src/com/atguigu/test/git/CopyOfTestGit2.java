b3278300f157882a2d3d5247414636e49bdbad84
iamxwj <2860532611@qq.com>
2016-07-08 22:52:02 +0800

    123
    123

--- src/com/atguigu/test/git/CopyOfTestGit2.java
+++ src/com/atguigu/test/git/CopyOfTestGit2.java
@@ -2,6 +2,7 @@ package com.atguigu.test.git;
 
 public class CopyOfTestGit2 {
 	public static void main(String[] args) {
+		System.out.println("Hello World");
 		System.out.println("Hello");
 		System.out.println("22222");
 		System.out.println("Hello World");
